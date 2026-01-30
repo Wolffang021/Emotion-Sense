e87dea2ea3f2f0970723fd6d7a0f7a5be64df998
SherlByom <dasp40406@gmail.com>
2026-01-30 17:09:00 +0530

    Backend initialization

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,16 @@
 # Flask Api integration here 
 # Entry point to model from UI
+import pickle
+
+fx = open("model.pkl", "rb")
+model = pickle.load(fx)
+
+# text = ["i am feeling very excited today"]
+# prediction = model.predict(text)
+
+# print(prediction)
+
+while True:
+    user_input = input("Enter you text:")
+    prediction = model.predict([user_input])
+    print("Emotion:",prediction)
